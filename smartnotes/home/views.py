1858874f272a898eb12cdec3c67bb5d3e0d532b8
Elnura Arslan <elnuraarslan@gmail.com>
2022-11-10 16:33:55 +0300

    first commit

--- smartnotes/home/views.py
+++ smartnotes/home/views.py
@@ -7,12 +7,18 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 
 
 class SignupView(CreateView):
     template_name = 'home/register.html'
     form_class = UserCreationForm
     success_url = 'notes/notes'
+    
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes.list')
+        return super().get(request, args, kwargs)
 
 
 class LogoutInterfaceView(LogoutView):
